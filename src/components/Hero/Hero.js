70ec781ec7105a03968ffed7b646fdb3bed37c05
VenkatSR-14 <53807312+VenkatSR-14@users.noreply.github.com>
2024-02-25 04:32:14 +0000

    created Hero Component

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -5,9 +5,20 @@ import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+  <Section row nonpadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Welcome To<br/>
+        My Personal Portfolio!
+      </SectionTitle>
+      <SectionText>
+        I love Problem Solving, Optimization and I have a unique blend of experiences in AI/ML, Data Engineering and Software Engineering
+      </SectionText>
+      <Button onClick = {() => window.location = 'https://drive.google.com/file/d/1cMl6fPgw7gp_NHk9FqMHjgo6Mc0F58GP/view'}>
+        View Resume
+      </Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
